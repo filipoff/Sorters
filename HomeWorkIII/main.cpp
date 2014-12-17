853dd0baa7430f369a46601007869275d5362030
filipoff <marto_1337@hotmail.com>
2014-12-17 14:06:29 +0200

    fixed main

--- HomeWorkIII/main.cpp
+++ HomeWorkIII/main.cpp
@@ -4,12 +4,13 @@ int main()
 {
 	Sorter<int> **sorters = new Sorter < int >*[5] ;
 	sorters[0] = new QuickSorter <int> ();
-	sorters[2] = new InsertionSorter <int>();
-	sorters[1] = new MergeSorter <int> ();
-	//sorters[2] = new InsertionSorter <int> ();
+	sorters[1] = new MergeSorter <int>();
+	sorters[2] = new HeapSorter <int>();
+	sorters[3] = new InsertionSorter <int> ();
 	
-	Tester<int> tester(sorters,2);
-	tester.getSummary(std::cout);
+
+	Tester<int> tester(sorters,4);
+	tester.getSummary(cout);
 	
 
 	return 0;
